7421dad1ccd3c43cad970a311eee7ae157035045
Sigita <sigita.zeb92@gmail.com>
2021-11-23 17:51:36 +0000

    Added mocks

--- shoppingBasket/basket.test.js
+++ shoppingBasket/basket.test.js
@@ -1,31 +1,30 @@
 const Basket = require("./basket");
-const Candy = require("./candy");
+
+const doubleMars = { getPrice: () => 4.99 };
+const doubleSkittles = { getPrice: () => 3.45 };
 
 describe("Basket.addItem", () => {
-  it('adds an item to the basket', () => {
-    let testBasket = new Basket()
-    let testCandyMars = new Candy("Mars", 4.99);
+  it("adds an item to the basket", () => {
+    let testBasket = new Basket();
 
-    expect(testBasket.addItem(testCandyMars)).toEqual([testCandyMars])
+    expect(testBasket.addItem(doubleMars)).toEqual([doubleMars]);
   });
 });
 
 describe("Basket.getTotalPrice", () => {
-  it('returns 0 when the basket is empty', () => {
-    let testBasket = new Basket()
-    expect(testBasket.getTotalPrice()).toBe(0)
+  it("returns 0 when the basket is empty", () => {
+    let testBasket = new Basket();
+    expect(testBasket.getTotalPrice()).toBe(0);
   });
 
-  it('returns the total price of all candies in the basket', () => {
-    let testBasket = new Basket()
-    let testCandyMars = new Candy("Mars", 4.99);
-    let testCandySkittles = new Candy("Skittles", 3.45);
+  it("returns the total price of all candies in the basket", () => {
+    let testBasket = new Basket();
 
-    testBasket.addItem(testCandyMars);
+    testBasket.addItem(doubleMars);
     expect(testBasket.getTotalPrice()).toBe(4.99);
-    testBasket.addItem(testCandySkittles);
+    testBasket.addItem(doubleSkittles);
     expect(testBasket.getTotalPrice()).toBe(8.44);
-    testBasket.addItem(testCandySkittles);
+    testBasket.addItem(doubleSkittles);
     expect(testBasket.getTotalPrice()).toBe(11.89);
   });
 });
